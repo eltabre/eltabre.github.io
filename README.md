89ed788ffa4ea63ea343b2ff0eabc5bfa8f999d9
eltabre <taberf@vt.edu>
2019-06-14 21:19:07 -0600

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-## Personal Website
-This is my personal website project. I used Tim Bakers react website template to start out with
-
-### TODO
-1. Update wording on site
-2. Change Font color on homepage
-3. Get download and contact form working
-4. Remove Jquery from site, try using other react libraries
---
+++ precache-manifest.76953f036caa05e16d8a04c24b29bfec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5627a80e95630632e56a",
+    "url": "/static/js/main.5627a80e.chunk.js"
+  },
+  {
+    "revision": "89ea3027e09d64b92383",
+    "url": "/static/js/1.89ea3027.chunk.js"
+  },
+  {
+    "revision": "5627a80e95630632e56a",
+    "url": "/static/css/main.2d4dfd2c.chunk.css"
+  },
+  {
+    "revision": "7368e1f5be042750a9cece6d71beab6c",
+    "url": "/index.html"
+  }
+];
