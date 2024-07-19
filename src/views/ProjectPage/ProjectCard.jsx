1cd2d7d5bcd8cabae7dad906c017a67c43e23c1b
eltabre <taberf@vt.edu>
2024-07-18 20:05:36 -0600

    push from laptop

--- src/views/ProjectPage/ProjectCard.jsx
+++ src/views/ProjectPage/ProjectCard.jsx
@@ -3,24 +3,22 @@ import './ProjectCard.css'; // make sure you have the correct path for your CSS 
 
 function ProjectCard({ id, title, description, imageUrl, isExpanded, onCardClick }) {
     return (
-      <div className={`project-card ${isExpanded ? 'expanded' : ''}`} onClick={onCardClick}>
-        <div className="card-inner">
-          {isExpanded ? (
-            <div className="expanded-content">
-              <h2>{title}</h2>
-              <img src={imageUrl} alt={title} />
-              <p>{description}</p>
-              {/* You can add more content here that you want to show when the card is expanded */}
-            </div>
-          ) : (
-            <div className="card-front">
-              {/* Default card content */}
-              <img src={imageUrl} alt={title} />
-              <h3>{title}</h3>
-              <p>{description}</p>
-            </div>
-          )}
-        </div>
+      <div className='card'>
+        <img src="https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80" alt="" />
+        <div className="card-content">
+      <h2>
+        Card Heading
+      </h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.
+      </p>
+      <a href="#" className="button">
+        Find out more 
+        <span className="material-symbols-outlined">
+          arrow_right_alt
+        </span>
+      </a>
+    </div>
       </div>
     );
   }
--- src/views/ProjectPage/ProjectPage.jsx
+++ src/views/ProjectPage/ProjectPage.jsx
@@ -6,9 +6,7 @@ function ProjectPage() {
   const [expandedCard, setExpandedCard] = useState(null);
 
   const projects = [
-    { id: 1, title: "Project 1", description: "Description of Project 1", imageUrl: "/path/to/image1.jpg" },
-    { id: 2, title: "Project 2", description: "Description of Project 2", imageUrl: "/path/to/image2.jpg" },
-    // Add more projects as needed
+    { id: 1, title: "Project 1", description: "Description of Project 1", imageUrl: "/path/to/image1.jpg" }
   ];
 
   const handleCardClick = (projectId) => {
